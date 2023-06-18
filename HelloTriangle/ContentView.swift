c764a816c2f3ae28a2e070050bc48e149dcdba37
Karthik Iyer <Karthik.iyer2@yandex.com>
2023-06-18 19:33:24 +0530

    Draw triangle
    Signed-off-by: Karthik Iyer <Karthik.iyer2@yandex.com>

--- HelloTriangle/ContentView.swift
+++ HelloTriangle/ContentView.swift
@@ -6,17 +6,34 @@
 //
 
 import SwiftUI
+import MetalKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ContentView: NSViewRepresentable {
+    
+    func makeCoordinator() -> Renderer {
+        Renderer(self)
     }
+    
+    func makeNSView(context: NSViewRepresentableContext<ContentView>) -> MTKView {
+        let mtkView = MTKView()
+        mtkView.delegate = context.coordinator
+        mtkView.preferredFramesPerSecond = 60
+        mtkView.enableSetNeedsDisplay = true
+        
+        if let metalDevice = MTLCreateSystemDefaultDevice() {
+            mtkView.device = metalDevice
+        }
+        
+        mtkView.framebufferOnly = false
+        mtkView.drawableSize = mtkView.frame.size
+        
+        return mtkView
+    }
+    
+    func updateNSView(_ nsView: MTKView, context: NSViewRepresentableContext<ContentView>) {
+    
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ HelloTriangle/definitions.h
@@ -0,0 +1,19 @@
+//
+//  definitions.h
+//  HelloTriangle
+//
+//  Created by Karthik Iyer on 18/06/23.
+//
+
+#ifndef definitions_h
+#define definitions_h
+
+#include <simd/simd.h>
+
+struct Vertex {
+    vector_float2 position;
+    vector_float4 color;
+};
+
+
+#endif /* definitions_h */
