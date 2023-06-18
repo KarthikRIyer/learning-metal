08217f36c72cc41f8fd6d856085fb541adfd9d07
Karthik Iyer <Karthik.iyer2@yandex.com>
2023-06-18 12:46:08 +0530

    Initial Commit

---
+++ HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by Karthik Iyer on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
